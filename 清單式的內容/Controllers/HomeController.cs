b6b3233c82fdc690477a96efb74549c6456ab80f
auxo <auxo@mail.ndmctsgh.edu.tw>
2018-09-19 19:21:25 +0800

    DropDownList介紹與傳值

--- 清單式的內容/Controllers/HomeController.cs
+++ 清單式的內容/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using 清單式的內容.Models;
 
 namespace 清單式的內容.Controllers
 {
@@ -15,6 +16,17 @@ namespace 清單式的內容.Controllers
             //CheckBox RadioButton
             string[] ary1 = { "日本", "美國", "台灣", "韓國" };
             ViewBag.ary1 = ary1;
+            ViewBag.ary2 = new SelectList(ary1);
+            ViewBag.ary3 = new SelectList(ary1, "台灣");
+
+            List<UserInfo> userlist = new List<UserInfo>()
+            {
+                new UserInfo(){ID=1, Name="Tony", Age=33},
+                new UserInfo(){ID=1, Name="Mary", Age=23},
+                new UserInfo(){ID=1, Name="John", Age=13}
+            };
+            ViewBag.ary4 = new SelectList(userlist, "ID", "Name", userlist[0].ID);
+
             return View();
         }
     }
---
+++ 清單式的內容/Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace 清單式的內容.Models
+{
+    public class UserInfo
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
