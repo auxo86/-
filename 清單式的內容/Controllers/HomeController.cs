008d121944b396d23b7babc91c6dfe04fa7de5a2
auxo <auxo@mail.ndmctsgh.edu.tw>
2018-09-19 19:01:17 +0800

    介紹helper

---
+++ 清單式的內容/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 清單式的內容.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //DropDownBox ListBox
+            //CheckBox RadioButton
+            string[] ary1 = { "日本", "美國", "台灣", "韓國" };
+            ViewBag.ary1 = ary1;
+            return View();
+        }
+    }
+}
